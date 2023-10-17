eed66fbd638ab31643b2f3666470979b0f19cd3b
vincentnam <dang.vincentnam@gmail.com>
2023-10-17 16:52:47 +0200

    First GUI version

--- src/index.js
+++ src/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Root from "./routes/root";
 import reportWebVitals from './reportWebVitals';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import ErrorPage from "./error-page";
+import Root_page from "./routes/root";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root_page />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/",
+        element: <Root_page />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/",
+        element: <Root_page />,
+        errorElement: <ErrorPage />,
+    },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+      <RouterProvider router={router} />
+
+
   </React.StrictMode>
 );
 
